e8322a555ec2472ac09a0c9dd05405347c61dcc3
IJumpAround <ventrn09@gmail.com>
2021-12-02 23:59:40 -0500

    Add puzzle input downloader

---
+++ aoc/puzzles/day_3/solve_day_3.py
@@ -0,0 +1,26 @@
+import os
+from pathlib import Path
+
+from aoc.utils.consts import SAMPLE_FILE, INPUT_FILE
+
+os.environ['LOG_LEVEL'] = 'DEBUG'
+from aoc import logger
+from aoc.utils import input_loader
+
+
+def solve(day, sample):
+    logger.info(f"Running with {SAMPLE_FILE if sample else INPUT_FILE}")
+    answer = None
+
+    list_input = input_loader.load_file_as_list(day, sample)
+    string_input = input_loader.load_file_as_string(day, sample)
+
+
+
+    logger.info(f"The answer is {answer}\n")
+
+
+if __name__ == '__main__':
+    _day_num = int(Path(__file__).stem.split('_')[-1])
+    solve(_day_num, sample=False)
+    solve(_day_num, sample=True)
